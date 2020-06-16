b047f82303c8be4956d961dad6525e99b5c079a1
etokohei <kohei.eto@rakus-partners.co.jp>
2020-06-16 17:10:54 +0900

    exam04フォームとドメインを作成

--- src/main/java/com/example/controller/Exam04Controller.java
+++ src/main/java/com/example/controller/Exam04Controller.java
@@ -1,11 +1,20 @@
 package com.example.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.form.UserForm;
 
 @Controller
 @RequestMapping("/exam4")
 public class Exam04Controller {
+	
+	@ModelAttribute
+	public UserForm setUpForm() {
+		return new UserForm();
+	}
+	
 	@RequestMapping("")
 	public String index() {
 		return "exam04";
@@ -14,5 +23,7 @@ public class Exam04Controller {
 	public String output() {
 		return "exam04-result";
 	}
+	
+	
 
 }
